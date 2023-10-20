b34507bc79b860d3a1654007a3a2cb0e6d30a335
Daniel Castillo <d.castillo.marfull@accenture.com>
2023-10-20 17:24:57 +0200

    feat(translations): adds translations

---
+++ src/translations/constants.js
@@ -0,0 +1,13 @@
+import { CONSTANTS } from "@constants";
+
+export const TRANSLATIONS = {
+  noResults: "No results",
+  searchSomething: "searchSomething",
+  guitarChords: "GUITAR chords",
+  emptySearch: "Please enter some search criteria.",
+  wrongChordsPattern: "Please enter valid search criteria.",
+  searchGuitarChords: "Search guitar chords",
+  [`${CONSTANTS.search}${CONSTANTS.label}`]: "Type a chord:",
+  [`${CONSTANTS.search}${CONSTANTS.placeholder}`]:
+    "C, Bm, Dsus, E7, Fmaj, G11...",
+};
