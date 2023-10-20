0a696e9b6ae40df1aebc7c477b9490096adf84d6
Daniel Castillo <d.castillo.marfull@accenture.com>
2023-10-20 17:12:49 +0200

    test(testing): add tests

---
+++ src/components/chord/chord.component.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { Chord } from "./chord.component";
+import mock from "./chord.mock.json";
+import styles from "./chord.module.css";
+import { setChordName } from "./chord.utils";
+
+describe("Chord", () => {
+  it("renders", async () => {
+    const { container } = render(<Chord chord={mock} />);
+    const { chordName, strings } = mock;
+    expect(await screen.findAllByText(setChordName(chordName))).toHaveLength(1);
+    expect(await screen.findAllByText(strings)).toHaveLength(1);
+    expect(container.getElementsByClassName(styles.img).length).toBe(1);
+  });
+});
